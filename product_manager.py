e22238f1f56c688ce32febc33243ba861b861042
Milos Jovanovic <jovanovicmilos26011997@gmail.com>
2025-04-03 23:51:03 +0200

    Added remove_product function in Product Manager

--- product_manager.py
+++ product_manager.py
@@ -22,4 +22,19 @@ class ProductManager:
         for product in self.products:
             total += product.price * product.quantity
         print(f"Ukupna vrednost svih proizvoda je {total} RSD.")
+        
+    def remove_product_by_name(self, name):
+        product_to_remove = None
+        for product in self.products:
+            if product.name.lower() == name.lower():
+                product_to_remove = product
+                break
+        
+        if product_to_remove:
+            self.products.remove(product_to_remove)
+            print(f"Proizvod '{name}' je uspešno uklonjen.")
+        else:
+            print(f"Proizvod sa imenom '{name}' nije pronađen.")
+            
+
 
